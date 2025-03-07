1d891b71a4bb1dfb5318e7b75023c43890b21d2e
Okpala Emmanuel <140505369+merison200@users.noreply.github.com>
2025-03-07 21:55:05 +0100

    Add files via upload

---
+++ src/test/java/emma_media/EmmaEcommerceStoreApplicationTest.java
@@ -0,0 +1,17 @@
+package emma_media;
+
+import static org.hibernate.validator.internal.util.Contracts.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EmmaEcommerceStoreApplicationTest {
+
+	@Test
+	public void ItShouldPassForNoReason() {
+
+	}
+
+}
